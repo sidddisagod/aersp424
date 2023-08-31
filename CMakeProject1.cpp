01147cba2bfdbe576d648b8a74f1f520e7bf96d7
siddisagod <sfp5478@psu.edu>
2023-08-31 18:02:01 -0400

    this is an update for quiz 2

---
+++ CMakeProject1.cpp
@@ -0,0 +1,13 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+int main()
+{
+
+	cout << "My name is Siddharth Premnath." << endl;
+	return 0;
+}
